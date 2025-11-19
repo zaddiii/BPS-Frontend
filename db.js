9f9ebabda84d51f52db640d93c011877d62fd4e5
乙ﾑddﾘ <joelomoemmanuel96@gmail.com>
2025-11-19 19:39:30 +0100

    Refactor MongoDB connection to use dotenv
    Updated MongoDB connection to use environment variable for URI and added connection options.

--- db.js
+++ db.js
@@ -1,14 +1,18 @@
 
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+require("dotenv").config(); // Load .env variables
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect("mongodb://localhost:27017/resultSystem"); 
-        console.log("MongoDB Connected");
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
-export default connectDB;
+module.exports = connectDB;

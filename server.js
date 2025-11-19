a786c80b381635e14500dd1d9d60da25faf30e67
乙ﾑddﾘ <joelomoemmanuel96@gmail.com>
2025-11-19 20:07:55 +0100

    Updated server.js to run with model native driver instead of mongooose

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 
 import express from "express";
 import cors from "cors";
-import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 import studentRoute from "./routes/studentRoute.js";
 import resultRoute from "./routes/resultRoute.js";
 
-// connect DB
-connectDB();
+dotenv.config();
+
+// Connect to MongoDB
+connectDB(); // initializes connection to BPS database
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// routes
+// Routes
 app.use("/api/students", studentRoute);
 app.use("/api/results", resultRoute);
 
+// Test route
 app.get("/", (req, res) => {
-    res.send("Result System API Running...");
+  res.send("Result System API Running...");
 });
 
-// start server
-app.listen(5000, () => console.log("Server running on port 5000"));
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
